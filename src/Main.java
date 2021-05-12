eb98244b997cdf677cccebe71013bd3cb8f72f2e
Guvanch <sylapov@itu.edu.tr>
2021-05-12 17:30:43 +0300

    Final work is done

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@ public class Main {
 	// write your code here
         System.out.println("Hello Git Last Day");
         //B21 is so excited
+        //B21 is doing well
+        //B21 is great
     }
 }
